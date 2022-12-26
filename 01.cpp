3299df7c8c135ec8c5636cf704feefc94206ee18
Kim <akk0425@naver.com>
2022-12-26 14:16:48 +0900

    with teacher

--- 01.cpp
+++ 01.cpp
@@ -7,14 +7,19 @@ int main()
 	int A = 0;
 	int B = 0;
 	int C = 0;
-	int D = 0;
+	
+	float x1 = 0;
+	float x2 = 0;
 
 	cout << "2차방식 방정식 근의 공식을 구해보세요"<<endl ;
 	cin >> A >> B >> C;
 
-	D = ((-1 * B) +- ((B * B) - (4 * A * C))) / (2 * A);	// 루트를 어떻게 계산하지?
-
-	cout << "답은? " << D << endl;	// 폭 to the 망.
+	x1 = ((-1 * B) + (float)sqrt(B * B - 4 * A * C)) / (2 * A);
+	x2 = ((-1 * B) - (float)sqrt(B * B - 4 * A * C)) / (2 * A);
+	
+	cout << "답은? " << endl;
+	cout << x1 << endl;
+	cout << x2 << endl;
 
 	return 0;
 }
